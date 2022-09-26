a8fc4755e8189a50d2eed49226a5223e85d161e9
Ape-Box <alessio.peternelli@gmail.com>
2022-09-26 21:34:08 +0100

    add further parsing

--- tools/scrapers/oreilly/index.ts
+++ tools/scrapers/oreilly/index.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import { load as cheerioLoad } from 'cheerio';
 import { promises as fsPromises } from 'fs';
 import { join } from 'path';
-import { stringify } from 'querystring';
 import ShortUniqueId from 'short-unique-id';
 
 interface Publisher {
@@ -48,40 +47,100 @@ export class OreillyScraper {
     async scrapeTitle(url: string): Promise<Book> {
         await sleep(1000);
         console.log(`[${time()}] Scraping {url}`, url);
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
 
-        const title = $('.t-title').text().trim();
-        // const authors = $('.author-name');
-        // const authors = $('.author-name').map((element) => {
-        //     return $(element).text().trim();
-        // });
-        const authors = $('.author-name').each((element) => {
-            console.log('=== {element}', typeof (element), JSON.stringify(element));
-            // const text = $(element).text().trim();
-            // console.log('=== {text}', typeof (text), JSON.stringify(text));
-        });
-        console.log(`[${time()}] Scraped data:`);
+        // === O'Reilly =======================================================
+        const oreResp = await axios.get(url);
+        const $ore = cheerioLoad(oreResp.data);
+
+        const title = $ore('.t-title').text().trim();
+        const authors = $ore('.author-name').map((_, element) => {
+            return $ore(element).text().trim();
+        }).toArray();
+        const isbn_13 = $ore('.t-isbn:last()').text().trim();
+        const release_date = $ore('.t-release-date').text().trim().replace("Released ", "");
+        const oreillyLink = oreResp.request.res.responseUrl;
+
+        console.log(`[${time()}] Scraped data {url}:`, url);
         console.log('  title: {title}', title);
-        console.log('  authors: {authors}', typeof (authors), JSON.stringify(authors));
+        console.log('  authors: {authors}', JSON.stringify(authors));
+        console.log('  isbn_13: {isbn_13}', isbn_13);
+        console.log('  release_date: {release_date}', release_date);
+        console.log('  oreillyLink: {oreillyLink}', oreillyLink);
+        console.log('\n\n');
 
-        const uid = new ShortUniqueId({ length: 10 });
+
+        // === ISBN ===========================================================
+        const isbnSearchUrl = `https://isbnsearch.org/isbn/${isbn_13}`;
+        const isbResp = await axios.get(isbnSearchUrl);
+        const $isb = cheerioLoad(isbResp.data);
+
+        const isbn_10 = $isb('.bookinfo').find('p:nth(1) a').text().trim();
+        const edition = parseInt($isb('.bookinfo').find('p:nth(3)').text().replace("Edition: ", "").trim(), 10);
+        const binding = $isb('.bookinfo').find('p:nth(4)').text().replace("Binding: ", "").trim();
+        const publication_date = $isb('.bookinfo').find('p:nth(6)').text().replace("Published: ", "").trim();
+
+        console.log(`[${time()}] Scraped data {url}:`, isbnSearchUrl);
+        console.log('  isbn_10: {isbn_10}', isbn_10);
+        console.log('  edition: {edition}', edition);
+        console.log('  binding: {binding}', binding);
+        console.log('  publication_date: {publication_date}', publication_date);
+        console.log('\n\n');
+
+
+        // === Open Library ===================================================
+        const olSearchUrl = `https://openlibrary.org/isbn/${isbn_13}`;
+        // const olResp = await axios.get(olSearchUrl);
+        // const $ol = cheerioLoad(olResp.data);
+
+        // const p = $ol('.edition-pages').text().trim();
+        // const pages = parseInt(p, 10);
+        // const languages = $ol('span[itemprop=inLanguage] > a').map((_, element) => {
+        //     return $ore(element).text().trim();
+        // }).toArray();
+
+        // console.log(`[${time()}] Scraped data {url}:`, olSearchUrl);
+        // console.log('  pages: {pages}', pages);
+        // console.log('  languages: {languages}', languages);
+        // console.log('\n\n');
+
+        const uid = new ShortUniqueId({ length: 6 });
 
         return {
-            id: `${uid()}-${title.replace(/^[A-z0-9]+/i, '-').toLowerCase()}`,
+            id: `${uid()}-${title.toLowerCase().replace(/[^a-z0-9]+/gimsu, '-')}`,
             title,
-            authors: [],
-            isbn_13: "string",
-            isbn_10: "string",
-            editions: 0,
-            binding: "string",
-            publishers: [],
-            release_date: "string",
-            publication_date: "string",
-            pages: 0,
+            authors: authors,
+            isbn_13: isbn_13,
+            isbn_10: isbn_10,
+            editions: edition,
+            binding: binding,
+            publishers: [
+                {
+                    "entity": "O'Reilly Media, Inc.",
+                    "website": "https://www.oreilly.com"
+                }
+            ],
+            release_date: release_date,
+            publication_date: publication_date,
+            pages: -1,
             languages: [],
-            links: [],
+            links: [
+                {
+                    "type": "publisher",
+                    "address": oreillyLink
+                },
+                {
+                    "type": "amazon uk",
+                    "address": `https://www.amazon.co.uk/gp/product/${isbn_10}`
+                },
+                {
+                    "type": "isbn search",
+                    "address": isbnSearchUrl
+                },
+                {
+                    "type": "open library",
+                    "address": olSearchUrl
+                }
+            ],
             tags: [],
             description: "string",
         };
